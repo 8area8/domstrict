fb478145f7c26fc4e6c8bc23ad65d5dc0c4366f9
8area8 <mbriolet.ma@gmail.com>
2019-04-21 22:47:17 +0200

    Bugfix: replaced the dist path.

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class Get {
-    id(idName: string): HTMLElement;
-    class(idName: string): Element[];
-    query(selector: string): Element;
-    queryAll(selector: string): Element[];
-}
